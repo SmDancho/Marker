7dc1657111190f73c14429333a81081c74b8a978
Danil <dantereshenko69@gmail.com>
2023-04-19 00:25:22 +0600

    Refactor: axios instance

--- src/utils/axios/index.tsx
+++ src/utils/axios/index.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 export const instance = axios.create({
-  baseURL:import.meta.env.VITE_PROD_URL,
+  baseURL: import.meta.env.MODE === 'development' ? import.meta.env.VITE_REQUEST_URl : import.meta.env.VITE_PROD_URL,
   timeout: 1000,
 });
-
