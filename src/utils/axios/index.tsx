b90ebffdbddc6d6fe18d2b9684c0af96efb11a4a
Danil <dantereshenko69@gmail.com>
2023-04-28 23:19:11 +0600

    fix:fix post req fo add post

--- src/utils/axios/index.tsx
+++ src/utils/axios/index.tsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 export const instance = axios.create({
   baseURL: import.meta.env.MODE === 'development' ? import.meta.env.VITE_REQUEST_URl : import.meta.env.VITE_PROD_URL,
   timeout: 8000,
-  withCredentials:true,
+ 
 });
