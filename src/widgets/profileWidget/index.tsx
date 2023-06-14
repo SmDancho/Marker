d4f1fce308d91d468a6429f99e2418bbf5513ef8
Danil <dantereshenko69@gmail.com>
2023-06-14 19:44:01 +0600

    reafactor: change exit button on icon

--- src/widgets/profileWidget/index.tsx
+++ src/widgets/profileWidget/index.tsx
@@ -7,6 +7,8 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import TabPanel from '@mui/lab/TabPanel';
 import TabContext from '@mui/lab/TabContext';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { IconButton } from '@mui/material';
 
 import { useTranslation } from 'react-i18next';
 
@@ -60,12 +62,12 @@ export const ProfileWidget: FC<props> = ({
     <div>
       <div className="flex justify-between mt-5 items-center">
         <div className="flex items-center gap-2">
-          <Avatar alt={`${username}`} />
+          <Avatar />
           <div>
-            <div className="flex gap-3">
+            <div>
               <div className="font-bold">{username}</div>
               {isAdmin && (
-                <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                <span className="inline-flex items-center rounded-md bg-red-50 px-2  text-xs font-medium text-red-700  ring-inset ring-red-600/10">
                   Admin
                 </span>
               )}
@@ -74,39 +76,36 @@ export const ProfileWidget: FC<props> = ({
             <div>likes {userLikes[0]?.length}</div>
           </div>
         </div>
-        <div>
-          {createBtnVisible && (
-            <Button
-              variant="outlined"
-              onClick={() => {
-                dispatch(logout());
-              }}
-            >
-              {translate.t('logOut')}
-            </Button>
-          )}
-        </div>
+
+        {createBtnVisible && (
+          <IconButton
+            onClick={() => {
+              dispatch(logout());
+            }}
+          >
+            <LogoutIcon />
+          </IconButton>
+        )}
       </div>
-      <section className="mt-5 flex justify-between items-center">
-        <div>
-          <Link to={'/create'}>
-            <Button variant="outlined">{translate.t('createReview')}</Button>
-          </Link>
-        </div>
-        <Tabs
-          value={tabs}
-          onChange={handleChangeTabs}
-          aria-label="basic tabs example"
-        >
+      <section className="mt-5 flex flex-col justify-between  lg:flex-row lg:items-center">
+        <Tabs value={tabs} onChange={handleChangeTabs}>
           <Tab label={translate.t('posts')} value={TabsPropety.posts} />
           {isAdmin && (
             <Tab label={translate.t('users')} value={TabsPropety.users} />
           )}
         </Tabs>
+
+        <div className="flex gap-2 mt-5 lg:mt-0">
+          <div>
+            <Link to={'/create'}>
+              <Button variant="outlined">{translate.t('createReview')}</Button>
+            </Link>
+          </div>
+        </div>
       </section>
 
       <TabContext value={tabs}>
-        <TabPanel value={TabsPropety.posts}>
+        <TabPanel value={TabsPropety.posts} className="p-0">
           <section className="flex gap-5 flex-wrap m-auto">
             <UserPosts />
           </section>
